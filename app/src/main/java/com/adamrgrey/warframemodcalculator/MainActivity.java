099ae86285c2cd6dc26031b3b87636521a2ad0d5
AdamRGrey <adam.r.grey@gmail.com>
2015-01-24 16:21:20 -0500

    builds, runs, but wtf it fucked up my activity 100%

--- app/src/main/java/com/adamrgrey/warframemodcalculator/MainActivity.java
+++ app/src/main/java/com/adamrgrey/warframemodcalculator/MainActivity.java
@@ -2,15 +2,29 @@ package com.adamrgrey.warframemodcalculator;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    NumberPicker startingRankPicker;
-    NumberPicker targetRankPicker;
+    public static NumberPicker startingRankPicker;
+    public static NumberPicker targetRankPicker;
+    public static Spinner raritySpinner;
+    private static final String LogTag = "MainActivity";
+    private ListView outputListview;
+    private ArrayAdapter outputArrayAdapter;
+    private ArrayList<String> outputList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +32,43 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         setupNumberPickers();
-        //setupSpinner();
-        //calculate();
+        setupSpinner();
+        calculate();
+        renderOutput();
+    }
+
+    private void setupSpinner() {
+        raritySpinner = (Spinner) findViewById(R.id.raritySpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.rarities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        raritySpinner.setAdapter(adapter);
+
+        raritySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                calculate();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(), "WHAT. HOW. don't do that. select an actual rarity please.", Toast.LENGTH_SHORT);
+                Log.e(LogTag, "rarity spinner: nothing selected. wtf.");
+            }
+        });
+    }
+
+    public void calculate() {
+        //TODO: calculate
+        outputList = new ArrayList<>();
+        //TODO: populate output array
+        renderOutput();
+    }
+
+    private void renderOutput() {
+        outputListview = (ListView) findViewById(R.id.outputListView);
+        outputArrayAdapter = new ArrayAdapter<>(this.getBaseContext(), android.R.layout.simple_list_item_1, outputList);
+        outputListview.setAdapter(outputArrayAdapter);
     }
 
     private void setupNumberPickers() {
@@ -29,7 +78,24 @@ public class MainActivity extends ActionBarActivity {
         startingRankPicker.setMaxValue(10);
         targetRankPicker.setMinValue(0); //fallback in case somehow the minimum doesn't get updated. I doubt it's possible, but who knows
         targetRankPicker.setMaxValue(10);
-        //TODO: add listeners for events
+
+        startingRankPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if(newVal > targetRankPicker.getValue())
+                {
+                    targetRankPicker.setValue(newVal);
+                }
+                targetRankPicker.setMinValue(newVal);
+                calculate();
+            }
+        });
+        targetRankPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                calculate();
+            }
+        });
     }
 
 
@@ -42,11 +108,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
